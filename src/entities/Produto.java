e87ce280c7e429a9b945a7dc8593bbff311e1432
aemsEmerson <emersonsilver4@gmail.com>
2025-09-13 20:32:15 -0300

    Criando a classe Produto

---
+++ src/entities/Produto.java
@@ -0,0 +1,57 @@
+package entities;
+
+public class Produto {
+    private String nome;
+    private Double preco;
+    private Integer quantidade;
+
+    public Produto() {
+
+    }
+
+    public Produto(String nome, Double preco, Integer quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Double valorTotalEmEstoque() {
+        return preco * quantidade;
+    }
+
+    public void adicionarEmEstoque(int qtd) {
+        quantidade += qtd;
+    }
+
+    public void removerEstoque(int qtd) {
+        if (qtd > quantidade) {
+            System.out.println("Saldo insuficiente");
+        } else {
+            quantidade -= qtd;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format
+                ("DADOS: \n\tProduto : %s,\n\tPreço: %.2f,\n\tQuantidade em estoque: %d,\n\tValor Total no Estoque: %.2f\n\t",
+                        nome, preco, quantidade, valorTotalEmEstoque());
+    }
+}
