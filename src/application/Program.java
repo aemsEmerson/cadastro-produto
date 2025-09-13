f424110e98d9c509bb8adf80032e29c14b5dd9b3
aemsEmerson <emersonsilver4@gmail.com>
2025-09-13 20:39:51 -0300

    Primeiro teste na classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,25 @@
 package application;
 
+import entities.Produto;
+
+import javax.imageio.plugins.jpeg.JPEGImageReadParam;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o nome do Produto: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite o valor do Produto: ");
+        double preco = sc.nextDouble();
+        System.out.print("Digite a Quantidade: ");
+        int quantidade = sc.nextInt();
+
+        Produto produto = new Produto(nome, preco, quantidade);
+        System.out.println(produto);
+        sc.close();
     }
 }
